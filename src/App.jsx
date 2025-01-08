99e2c51908e257bcf67dc87f64830baad9976d54
Rakesh Penugonda <119618769+Rakesh-116@users.noreply.github.com>
2025-01-08 14:26:15 +0530

    Adding Client Files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black h-[100vh] flex flex-col justify-center">
+      <div className="flex justify-center">
+        <h1 className="text-white">Hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
